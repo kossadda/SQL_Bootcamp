08b71657ec5299c5d0cacc345dbc5dc7c9058c1d
kossadda <gabilov1997@gmail.com>
2024-03-24 17:17:12 +0500

    feat: 00x02 done

---
+++ SQL_beginner.Day00/src/ex02/day00_ex02.sql
@@ -0,0 +1,23 @@
+-- Using the SELECT statement using comparison signs (<=, >=)
+SELECT
+    p.name,
+    p.rating
+FROM
+    pizzeria AS p
+WHERE
+    p.rating >= 3.5
+AND p.rating <= 5.0
+ORDER BY
+    p.rating DESC;
+
+-- Using the SELECT statement using the BETWEEN keyword
+SELECT
+    p.name,
+    p.rating
+FROM
+    pizzeria AS p
+WHERE
+    p.rating BETWEEN 3.5
+                 AND 5.0
+ORDER BY
+    p.rating DESC;
