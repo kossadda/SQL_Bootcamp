dfcb769ca20fc0ba1c145df3ca95714132302b27
kossadda <gabilov1997@gmail.com>
2024-03-24 17:20:20 +0500

    feat: 00x03 done

---
+++ SQL_beginner.Day00/src/ex03/day00_ex03.sql
@@ -0,0 +1,9 @@
+SELECT DISTINCT
+    v.person_id
+FROM
+    person_visits AS v
+WHERE
+   (v.visit_date BETWEEN '2022-01-06' AND '2022-01-09')
+OR  v.pizzeria_id = 2
+ORDER BY
+    v.person_id DESC;
