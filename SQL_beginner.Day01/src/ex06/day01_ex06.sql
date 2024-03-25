080e27661035c9e55e160d5fe501361aa13d8ca0
kossadda <gabilov1997@gmail.com>
2024-03-26 00:32:16 +0500

    style: 01x06 correct tabs

--- SQL_beginner.Day01/src/ex06/day01_ex06.sql
+++ SQL_beginner.Day01/src/ex06/day01_ex06.sql
@@ -7,13 +7,13 @@ SELECT
       SELECT
             p.visit_date AS action_date,
             p.person_id AS person_id
-      FROM
+        FROM
             person_visits AS p
       INTERSECT
       SELECT
             o.order_date AS action_date,
             o.person_id AS person_id
-      FROM
+        FROM
             person_order AS o
       ORDER BY
             action_date ASC,
