c5a894a6822764c0dc5ec87476f2065c429a58a2
kossadda <gabilov1997@gmail.com>
2024-03-27 16:53:49 +0500

    feat: 02x03 done

---
+++ SQL_beginner.Day02/src/ex03/day02_ex03.sql
@@ -0,0 +1,18 @@
+SELECT
+      d.day::date AS missing_date
+  FROM
+      GENERATE_SERIES
+            (TIMESTAMP '2022-01-01', '2022-01-10', '1 day') AS d(day)
+  LEFT JOIN
+      (SELECT
+             pv.visit_date
+         FROM
+             person_visits AS pv
+        WHERE 
+             pv.person_id = 1
+             OR pv.person_id = 2
+      ) AS v ON (d.day = v.visit_date)
+ WHERE 
+      v.visit_date IS NULL
+ ORDER BY
+      d.day
