cd28a7c23a337345fbea7fa9397d309930201d41
kossadda <gabilov1997@gmail.com>
2024-03-29 20:41:09 +0500

    feat: 03x08 done

---
+++ SQL_beginner.Day03/src/ex08/day03_ex08.sql
@@ -0,0 +1,13 @@
+INSERT INTO menu
+VALUES (
+      (SELECT
+             COUNT(*)
+         FROM
+             menu) + 1,
+      (SELECT
+             pz.id
+         FROM
+             pizzeria AS pz
+        WHERE
+             pz.name = 'Dominos'),
+      'sicilian pizza', 900);
