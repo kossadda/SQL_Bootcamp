0a7892cad9f6678762f295f56dcbe86e3a967f7f
kossadda <gabilov1997@gmail.com>
2024-03-29 10:41:01 +0500

    fix: 02x01 change for generate series

--- SQL_beginner.Day02/src/ex01/day02_ex01.sql
+++ SQL_beginner.Day02/src/ex01/day02_ex01.sql
@@ -1,12 +1,8 @@
 SELECT
-      d.day AS missing_date
+      d.day::date AS missing_date
   FROM
-      (VALUES
-            (DATE '2022-01-01'), (DATE '2022-01-02'), (DATE '2022-01-03'),
-            (DATE '2022-01-04'), (DATE '2022-01-05'), (DATE '2022-01-06'),
-            (DATE '2022-01-07'), (DATE '2022-01-08'), (DATE '2022-01-09'),
-            (DATE '2022-01-10')
-      ) AS d(day)
+      GENERATE_SERIES
+      (TIMESTAMP '2022-01-01', '2022-01-10', '1 day') AS d(day)
   LEFT JOIN
       (SELECT
              pv.visit_date
