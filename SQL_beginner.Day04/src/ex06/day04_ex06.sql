cfbed43d42f9408acd34a56e6e70a4e14fa39a74
kossadda <gabilov1997@gmail.com>
2024-04-05 22:01:23 +0500

    feat: 04x06 done

---
+++ SQL_beginner.Day04/src/ex06/day04_ex06.sql
@@ -0,0 +1,16 @@
+CREATE MATERIALIZED VIEW
+      mv_dmitriy_visits_and_eats AS
+SELECT DISTINCT
+      pizzeria_name
+  FROM
+      person AS p(person_id, person_name)
+NATURAL JOIN
+      person_visits AS pv(visit_id)
+NATURAL JOIN
+      pizzeria AS pz(pizzeria_id, pizzeria_name)
+NATURAL JOIN
+      menu AS m(menu_id)
+ WHERE
+      visit_date = '2022-01-08'
+      AND person_name = 'Dmitriy'
+      AND price < 800;
