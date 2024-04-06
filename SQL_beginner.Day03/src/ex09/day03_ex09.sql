ced9e28ce051c9d049cfc5aa0a4978a8a1b70466
kossadda <gabilov1997@gmail.com>
2024-04-06 21:50:31 +0500

    style: 03x07-10 change format

--- SQL_beginner.Day03/src/ex09/day03_ex09.sql
+++ SQL_beginner.Day03/src/ex09/day03_ex09.sql
@@ -1,11 +1,12 @@
-INSERT INTO person_visits
+INSERT INTO
+      person_visits (id, person_id, pizzeria_id, visit_date)
 VALUES (
       (SELECT COUNT(*) FROM person_visits) + 1,
       (SELECT p.id FROM person AS p WHERE p.name = 'Denis'),
       (SELECT pz.id FROM pizzeria AS pz WHERE pz.name = 'Dominos'),
-      TIMESTAMP '2022-02-24'),
+      (TIMESTAMP '2022-02-24')),
       (
       (SELECT COUNT(*) FROM person_visits) + 2,
       (SELECT p.id FROM person AS p WHERE p.name = 'Irina'),
       (SELECT pz.id FROM pizzeria AS pz WHERE pz.name = 'Dominos'),
-      TIMESTAMP '2022-02-24');
+      (TIMESTAMP '2022-02-24'));
--- SQL_beginner.Day03/src/ex10/day03_ex10.sql
+++ SQL_beginner.Day03/src/ex10/day03_ex10.sql
@@ -1,11 +1,12 @@
-INSERT INTO person_order
+INSERT INTO
+      person_order (id, person_id, menu_id, order_date)
 VALUES (
       (SELECT COUNT(*) FROM person_order) + 1,
       (SELECT p.id FROM person AS p WHERE p.name = 'Denis'),
       (SELECT m.id FROM menu AS m WHERE m.pizza_name = 'sicilian pizza'),
-      TIMESTAMP '2022-02-24'),
+      (TIMESTAMP '2022-02-24')),
       (
       (SELECT COUNT(*) FROM person_order) + 2,
       (SELECT p.id FROM person AS p WHERE p.name = 'Irina'),
       (SELECT m.id FROM menu AS m WHERE m.pizza_name = 'sicilian pizza'),
-      TIMESTAMP '2022-02-24');
+      (TIMESTAMP '2022-02-24'));
