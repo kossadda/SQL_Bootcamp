e000f989d073feb91dff5b7a6dbd65bc8c9434b9
kossadda <gabilov1997@gmail.com>
2024-04-06 21:31:23 +0500

    feat: 05x04 done

---
+++ SQL_beginner.Day05/src/ex04/day05_ex04.sql
@@ -0,0 +1,22 @@
+CREATE UNIQUE INDEX
+      idx_menu_unique ON menu (pizzeria_id, pizza_name);
+
+SET ENABLE_SEQSCAN TO OFF;
+
+EXPLAIN ANALYZE
+SELECT
+      pizzeria_id,
+      pizza_name
+  FROM
+      menu
+ WHERE
+      pizzeria_id = 1;
+
+-- A unique index does not allow the creation of duplicates of the columns
+-- specified in the index. Let's check this by adding a new row to the "menu"
+INSERT INTO
+      menu
+VALUES
+      (21, 4, 'mushroom pizza', 1000);
+
+SET ENABLE_SEQSCAN TO OFF;
