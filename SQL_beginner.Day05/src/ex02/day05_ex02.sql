4904f3580939f9a001a49f25b0306e370bdc5e53
kossadda <gabilov1997@gmail.com>
2024-04-06 20:28:07 +0500

    feat: 05x02 done

---
+++ SQL_beginner.Day05/src/ex02/day05_ex02.sql
@@ -0,0 +1,13 @@
+CREATE INDEX idx_person_name ON person (UPPER(name));
+
+SET ENABLE_SEQSCAN TO OFF;
+
+EXPLAIN ANALYZE
+SELECT
+      *
+  FROM
+      person
+ WHERE
+      UPPER(name) = 'KATE';
+
+SET ENABLE_SEQSCAN TO ON;
