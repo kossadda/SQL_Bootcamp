e3fd7f4d33771fd422b6da4c7c1ccacb1030bf84
kossadda <gabilov1997@gmail.com>
2024-04-07 12:40:35 +0500

    feat: 05x05 correct comment

--- SQL_beginner.Day05/src/ex05/day05_ex05.sql
+++ SQL_beginner.Day05/src/ex05/day05_ex05.sql
@@ -5,8 +5,8 @@ CREATE INDEX
 
 SET ENABLE_SEQSCAN TO OFF;
 
--- The index from job 03 "idx_person_order_multi" is very similar to the index
--- of this job, except that the order_date is not tracked by any order, namely
+-- The index from task 03 "idx_person_order_multi" is very similar to the index
+-- of this task, except that the order_date is not tracked by any order, namely
 -- "2022-01-01". The below query is suitable for both indexes, but since the
 -- index we need is more focused on the required sample, the analyzer will
 -- prefer this index instead of the index from task 03
