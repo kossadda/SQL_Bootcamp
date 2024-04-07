6b542319f314acdfbe0ab050fa725b1817f4cf80
kossadda <gabilov1997@gmail.com>
2024-04-07 20:53:38 +0500

    style: correct values

--- SQL_beginner.Day03/src/ex08/day03_ex08.sql
+++ SQL_beginner.Day03/src/ex08/day03_ex08.sql
@@ -1,7 +1,7 @@
 INSERT INTO
       menu (id, pizzeria_id, pizza_name, price)
 VALUES (
-      (SELECT COUNT(*) FROM menu) + 12,
+      (SELECT COUNT(*) FROM menu) + 1,
       (SELECT pz.id FROM pizzeria AS pz WHERE pz.name = 'Dominos'),
       ('sicilian pizza'),
       (900));
--- SQL_beginner.Day03/src/ex09/day03_ex09.sql
+++ SQL_beginner.Day03/src/ex09/day03_ex09.sql
@@ -4,9 +4,9 @@ VALUES (
       (SELECT COUNT(*) FROM person_visits) + 1,
       (SELECT p.id FROM person AS p WHERE p.name = 'Denis'),
       (SELECT pz.id FROM pizzeria AS pz WHERE pz.name = 'Dominos'),
-      (TIMESTAMP '2022-02-24')),
+      ('2022-02-24')),
       (
       (SELECT COUNT(*) FROM person_visits) + 2,
       (SELECT p.id FROM person AS p WHERE p.name = 'Irina'),
       (SELECT pz.id FROM pizzeria AS pz WHERE pz.name = 'Dominos'),
-      (TIMESTAMP '2022-02-24'));
+      ('2022-02-24'));
