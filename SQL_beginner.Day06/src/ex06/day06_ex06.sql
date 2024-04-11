76286c19bdf976eb4a294b1236d71e149cd01944
kossadda <gabilov1997@gmail.com>
2024-04-11 14:28:12 +0500

    feat: 06x06 modify

--- SQL_beginner.Day06/src/ex06/day06_ex06.sql
+++ SQL_beginner.Day06/src/ex06/day06_ex06.sql
@@ -1,23 +1,17 @@
 CREATE SEQUENCE
       seq_person_discounts
  START
-      1;
-
-SELECT
-      SETVAL('seq_person_discounts', MAX(id))
-  FROM
-      person_discounts;
-
-SELECT
-      *
-  FROM
-      seq_person_discounts;
+      (SELECT MAX(id) FROM person_discounts);
 
  ALTER TABLE
       person_discounts
  ALTER COLUMN
       id SET DEFAULT NEXTVAL('seq_person_discounts');
 
+-- Please note that if the insertion fails, the sequence will still be
+-- incremented. To prevent incrementation, transactional ones should be
+-- used. Since we have not yet reached this topic, we will leave everything
+-- in its current form.
 INSERT INTO
       person_discounts (person_id, pizzeria_id, discount)
 VALUES
