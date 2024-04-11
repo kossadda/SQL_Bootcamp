d424021761ca27e3dc67070a5c55ffc79f1bbf7b
kossadda <gabilov1997@gmail.com>
2024-04-12 01:29:54 +0500

    feat: 06x06 modify

--- SQL_beginner.Day06/src/ex06/day06_ex06.sql
+++ SQL_beginner.Day06/src/ex06/day06_ex06.sql
@@ -1,8 +1,21 @@
+-- We will also describe the OWN BY keyword, which will allow the sequence to be
+-- set as the owner of the specified column. This way the sequence will be
+-- deleted when that column is deleted.
 CREATE SEQUENCE
-      seq_person_discounts
- START
-      (SELECT MAX(id) FROM person_discounts);
+      seq_person_discounts AS INT
+ OWNED BY
+      person_discounts.id;
 
+-- Since the table already contains rows, we will set the maximum id value in
+-- the last value of the sequence
+SELECT
+      SETVAL('seq_person_discounts', MAX(id))
+  FROM
+      person_discounts;
+
+-- Since our table already has rows and the type of the id attribute is not
+-- SERIAL, it is necessary to set the DEFAULT value for id in the form of
+-- NEXTVAL of sequence
  ALTER TABLE
       person_discounts
  ALTER COLUMN
@@ -15,7 +28,7 @@ CREATE SEQUENCE
 INSERT INTO
       person_discounts (person_id, pizzeria_id, discount)
 VALUES
-      (9, 2, 22);
+      (4, 4, 22);
 
 SELECT
       *
