238f9f49f4bf9a45fb958897ee5872a6fec641ef
kossadda <gabilov1997@gmail.com>
2024-04-15 21:43:52 +0500

    feat: 08x02 done

---
+++ SQL_beginner.Day08/src/ex02/day08_ex02.sql
@@ -0,0 +1,56 @@
+-- FIRST SESSION
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;;
+SHOW TRANSACTION ISOLATION LEVEL;
+
+-- SECOND SESSION
+BEGIN TRANSACTION ISOLATION LEVEL REPEATABLE READ;;
+
+-- FIRST SESSION
+SELECT
+      *
+  FROM
+      pizzeria
+ WHERE
+      name = 'Pizza Hut';
+
+-- SECOND SESSION
+SELECT
+      *
+  FROM
+      pizzeria
+ WHERE
+      name = 'Pizza Hut';
+
+-- FIRST SESSION
+UPDATE
+      pizzeria
+   SET
+      rating = 4.0
+ WHERE
+      name = 'Pizza Hut';
+
+-- SECOND SESSION: the table data will not be updated in the second session
+-- until the transaction of the first session is completed
+UPDATE
+      pizzeria
+   SET
+      rating = 3.6
+ WHERE
+      name = 'Pizza Hut';
+
+-- FIRST SESSION
+COMMIT;
+
+-- SECOND SESSION
+COMMIT;
+
+-- In this case, upon completion of the transaction of the first session,
+-- changes to the same tuple in the second will be accompanied by an error
+
+-- BOTH SESSIONS
+SELECT
+      *
+  FROM
+      pizzeria
+ WHERE
+      name = 'Pizza Hut';
