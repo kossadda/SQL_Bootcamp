a418ab3988875e219ce93772b61cc1dc96f567d3
kossadda <gabilov1997@gmail.com>
2024-04-17 13:39:16 +0500

    feat: 09x02 done

---
+++ SQL_beginner.Day09/src/ex02/day09_ex02.sql
@@ -0,0 +1,34 @@
+BEGIN;
+
+CREATE OR REPLACE FUNCTION 
+      fnc_trg_person_delete_audit()
+RETURNS
+      TRIGGER AS
+      $$ BEGIN
+      INSERT INTO
+            person_audit(type_event, row_id, name, age, gender, address)
+      SELECT
+            'D', OLD.*;
+      RETURN NULL;
+      END $$ 
+LANGUAGE plpgsql;
+
+CREATE TRIGGER
+      trg_person_delete_audit
+ AFTER DELETE ON
+      person FOR EACH ROW
+EXECUTE FUNCTION 
+      fnc_trg_person_delete_audit();
+
+DELETE FROM
+      person
+ WHERE
+      id = 10;
+
+SELECT
+      *
+  FROM
+      person_audit;
+
+ROLLBACK;
+COMMIT;
